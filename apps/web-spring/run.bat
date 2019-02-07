2fc2fbc5f067aad5e5625875ec1bd220815cc9cc
jinjavamojo <jinjavamojo@gmail.com>
2019-02-07 21:02:39 +0400

    small changes

--- apps/web-spring/run.bat
+++ apps/web-spring/run.bat
@@ -2,6 +2,7 @@ set CATALINA_HOME=D:/ll/apache-tomcat-8.5.34
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_161
 set JPDA_ADDRESS=9000
 set JPDA_TRANSPORT=dt_socket
+set "JAVA_OPTS=%JAVA_OPTS% -Dfile.encoding=UTF8"
 
 %CATALINA_HOME%/bin/catalina.bat jpda start
 
